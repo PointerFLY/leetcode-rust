b5a3a84f0d78872d3b8e281b8a92f8d9875b7174
Michael Ma <linghao.michael.ma@gmail.com>
2024-08-08 09:50:00 +0800

    Solve 885

--- src/s885.rs
+++ src/s885.rs
@@ -1,7 +1,47 @@
 struct Solution;
 
 impl Solution {
-    pub fn spiral_matrix_iii(rows: i32, cols: i32, r_start: i32, c_start: i32) -> Vec<Vec<i32>> {}
+    pub fn spiral_matrix_iii(rows: i32, cols: i32, r_start: i32, c_start: i32) -> Vec<Vec<i32>> {
+        let validate = |r: i32, c: i32| r >= 0 && r < rows && c >= 0 && c < cols;
+        let next_coord = |r: i32, c: i32, direction: i32| -> (i32, i32) {
+            match direction {
+                0 => (r, c + 1),
+                1 => (r + 1, c),
+                2 => (r, c - 1),
+                3 => (r - 1, c),
+                _ => (0, 0),
+            }
+        };
+
+        let mut coords: Vec<Vec<i32>> = vec![vec![r_start, c_start]];
+        let mut steps_left = 1;
+        let mut stride = 1;
+        let mut rotates = 0;
+        let mut direction = 0; // Right: 0, Down: 1, Left: 2, Up: 3
+        let mut cur_r = r_start;
+        let mut cur_c = c_start;
+
+        while coords.len() < (rows * cols) as usize {
+            (cur_r, cur_c) = next_coord(cur_r, cur_c, direction);
+            if validate(cur_r, cur_c) {
+                coords.push(vec![cur_r, cur_c]);
+            }
+
+            steps_left -= 1;
+            if steps_left == 0 {
+                direction = (direction + 1) % 4;
+
+                rotates += 1;
+                if rotates == 2 {
+                    stride += 1;
+                    rotates = 0;
+                }
+                steps_left = stride
+            }
+        }
+
+        coords
+    }
 }
 
 #[cfg(test)]
